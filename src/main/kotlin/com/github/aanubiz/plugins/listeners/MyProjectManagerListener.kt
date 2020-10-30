ae1fa7d202cf04066c3c1410c8b949679a52cf6a
GitHub Action <action@github.com>
2020-10-30 09:12:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/aanubiz/plugins/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/aanubiz/plugins/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aanubiz.plugins.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aanubiz.plugins.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/aanubiz/plugins/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aanubiz/plugins/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aanubiz.plugins.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aanubiz.plugins.MyBundle
 
 class MyApplicationService {
 
